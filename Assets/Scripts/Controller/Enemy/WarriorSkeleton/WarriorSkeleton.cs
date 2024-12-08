b0a663dfb0e5210e9cdd3fb382969ff0c8c4555d
Lebick <levickyou@gmail.com>
2024-12-09 00:51:55 +0900

    warrierComplete

--- Assets/Scripts/Controller/Enemy/WarriorSkeleton/WarriorSkeleton.cs
+++ Assets/Scripts/Controller/Enemy/WarriorSkeleton/WarriorSkeleton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class WarriorSkeleton : EnemyController
 {
@@ -71,24 +72,39 @@ public class WarriorSkeleton : EnemyController
         yield return new WaitForSeconds(25f / 60f);
 
         Vector3 startPos = transform.position;
-        Vector3 targetPos = (player.position - transform.position).normalized * skillDistance;
+        Vector3 targetPos = startPos + (player.position - transform.position).normalized * skillDistance;
 
         float progress = 0f;
 
         skillEffect.Play();
 
-        while(progress <= 1f)
+        CircleCollider2D circle = GetComponent<CircleCollider2D>();
+
+        Vector2 lastestPos = startPos;
+
+        while (progress <= 1f)
         {
             progress += Time.deltaTime * 2f;
             float acceleration = Mathf.Sqrt(1 - Mathf.Pow(progress - 1, 2));
 
             transform.position = Vector3.Lerp(startPos, targetPos, acceleration);
 
+            NavMeshHit hit;
+            bool isOnNavMesh = NavMesh.SamplePosition(transform.position, out hit, 0.1f, NavMesh.AllAreas);
+
+            if (!isOnNavMesh) //이동 가능한 영역에서 벗어났으면
+            {
+                transform.position = lastestPos; //최근 이동한 위치로 이동
+                break; //탈출
+            }
+
+            lastestPos = transform.position;
+
             yield return null;
         }
         skillEffect.Stop();
 
-        yield return new WaitForSeconds(20f / 60f);
+        yield return new WaitForSeconds((20f / 60f) - (1f - progress) / 2f);
 
         isUsingSkill = false;
     }
